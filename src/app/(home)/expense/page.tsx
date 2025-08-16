95d511d3788822a11172f537a5489c6a31ac2352
foisal404 <foisalahmmed2@gmail.com>
2025-08-16 18:28:58 +0600

    get expences api connect with ui

--- src/app/(home)/expense/page.tsx
+++ src/app/(home)/expense/page.tsx
@@ -1,6 +1,8 @@
 "use client";
-
-import { useState } from "react";
+import ExpencesForm from "@/components/ExpencesForm";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 interface Expense {
   id: number;
@@ -41,65 +43,40 @@ export default function ExpensePage() {
   const handleDelete = (id: number) => {
     setExpenses(expenses.filter((exp) => exp.id !== id));
   };
+  useEffect(() => {
+    const fetchExpenses = async () => {
+      try {
+        await axios
+          .get(`${process.env.NEXT_PUBLIC_API_URL}/api/expenses`, {
+            withCredentials: true,
+          })
+          .then((res) => setExpenses(res.data));
+      } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+          toast(error.response?.data?.message || "Failed to get expense data", {
+            type: "error",
+          });
+        } else {
+          toast("An unexpected error occurred", {
+            type: "error",
+            theme: "dark",
+          });
+        }
+      }
+    };
+    fetchExpenses();
+  }, []);
 
   return (
     <div className="max-w-3xl mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6">Expenses</h1>
 
-      {/* Add Expense Form */}
-      <form
+      <ExpencesForm
         onSubmit={handleSubmit}
-        className="space-y-4 mb-8 bg-white shadow-md p-4 rounded-lg"
-      >
-        <input
-          type="text"
-          name="title"
-          placeholder="Title"
-          value={form.title || ""}
-          onChange={handleChange}
-          required
-          className="w-full border px-3 py-2 rounded"
-        />
+        onChange={handleChange}
+        form={form}
+      />
 
-        <input
-          type="number"
-          name="amount"
-          placeholder="Amount"
-          value={form.amount || ""}
-          onChange={handleChange}
-          required
-          className="w-full border px-3 py-2 rounded"
-        />
-
-        <select
-          name="category"
-          value={form.category}
-          onChange={handleChange}
-          className="w-full border px-3 py-2 rounded"
-        >
-          <option>Food</option>
-          <option>Transport</option>
-          <option>Shopping</option>
-          <option>Others</option>
-        </select>
-
-        <input
-          type="date"
-          name="date"
-          value={form.date || ""}
-          onChange={handleChange}
-          className="w-full border px-3 py-2 rounded"
-        />
-
-        <button
-          type="submit"
-          className="w-full bg-lime-600 text-white py-2 rounded hover:bg-lime-700"
-        >
-          Add Expense
-        </button>
-      </form>
-
-      {/* Expense List */}
       <div className="space-y-4">
         {expenses.length === 0 ? (
           <p className="text-gray-600">No expenses yet.</p>
