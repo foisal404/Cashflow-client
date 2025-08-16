1c32b7c5b8550da977adc4c6371dbaf46eb4a38a
foisal404 <foisalahmmed2@gmail.com>
2025-08-16 16:02:10 +0600

    feat: React toast container add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ToastContainer } from "react-toastify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="text-gray-900 bg-[#E7E8E0] min-h-screen ">
+          <ToastContainer />
           {children}
         </div>
       </body>
